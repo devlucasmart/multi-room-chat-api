6e0b24cc49c42309d7b85d39408b84e1c5cfe087
Lucas Martins <lucas.martins.fsa@gmail.com>
2024-05-17 14:35:49 -0300

    implementa valida campo nome ou apelido

--- config/server.js
+++ config/server.js
@@ -5,9 +5,9 @@ const expressValidator = require('express-validator')
 
 var app = express();
 
-app.use(express.static('./app/public'))
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
+app.use(express.static('./app/public'))
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(expressValidator());
