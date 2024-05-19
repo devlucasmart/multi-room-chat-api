ea92028b84666a043eddf1d5a7d47e09d11d69d7
Lucas Martins <lucas.martins.fsa@gmail.com>
2024-05-19 18:38:08 -0300

    implementa atualiza lista de participantes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # multi-room-chat-api
+
+#Comando para executar o container Docker
+
+docker run --name chat -p 3000:3000 lord143/chat
--- app.js
+++ app.js
@@ -1,5 +1,42 @@
 var app = require('./config/server')
 
-app.listen(3000, function(){
+var server = app.listen(3000, function(){
     console.log('Servidor ON')
+});
+
+var io = require('socket.io')(server);
+app.set('io', io);
+
+/* Criar a conexão por WebSocket */
+io.on('connection', function(socket){
+    console.log('Usuário conectou');
+
+    socket.on('disconnect', function(){
+        console.log('Usuário desconectou');
+    });
+
+    socket.on('msgParaServidor', function(data){
+        /* dialogo */
+        socket.emit(
+            'msgParaCliente',
+            { apelido: data.apelido, mensagem: data.mensagem }
+        );
+        socket.broadcast.emit(
+            'msgParaCliente',
+            { apelido: data.apelido, mensagem: data.mensagem }
+        );
+        
+        /* participantes */
+        if(parseInt(data.apelido_atualizado_nos_clientes) == 0){
+            socket.emit(
+                'participantesParaCliente',
+                { apelido: data.apelido}
+            );
+            socket.broadcast.emit(
+                'participantesParaCliente',
+                { apelido: data.apelido}
+            );
+        }
+        
+    });
 });
